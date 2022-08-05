0ba819475bd168156b5b2630921c0a242c5a5ea3
Marco Andorno <27402195+mksoc@users.noreply.github.com>
2022-08-05 18:44:00 +0200

    Updated plotting functions

--- src/main.py
+++ src/main.py
@@ -1,8 +1,27 @@
 import data_preparation
 import single_var_analysis
 
-# single_var_analysis.response_rate(data_preparation.df_all['Customer'])
-single_var_analysis.plot_bar(data_preparation.df_yes['Frequency'], 's02_frequency.pdf', bottom_buffer=0.3)
-# single_var_analysis.purchase_location(data_preparation.df_yes['PurchaseLocation'])
-single_var_analysis.plot_bar(data_preparation.df_yes['BrandKnownFor'], 's04_brand_known_for.pdf', tight=True)
-# single_var_analysis.receipt(data_preparation.df_yes)
+# Customer yes/no
+single_var_analysis.plot_pie(data_preparation.df_all['Customer'], 's01_response_rate.pdf', autopct='%1.1f%%', startangle=90, ylabel='')
+
+# Buying frequency
+single_var_analysis.plot_bar(data_preparation.df_yes['Frequency'], 's02_frequency.pdf', rot=45)
+
+# Purchase channel
+single_var_analysis.purchase_location(data_preparation.df_yes['PurchaseLocation'], 's03_purchase_location.pdf')
+
+# Brand known for how long
+single_var_analysis.plot_bar(data_preparation.df_yes['BrandKnownFor'], 's04_brand_known_for.pdf', rot=45)
+
+# Average receipt amount
+single_var_analysis.plot_hist(data_preparation.df_yes['ReceiptAmount'], 's05_receipt.pdf', bins=8)
+
+# Total purchases
+single_var_analysis.plot_bar(data_preparation.df_yes['AllPurchases'].explode(), 's06_all_purchases.pdf',
+    orientation='h', bar_label=False, sort='values', width=0.8, figsize=(10, 8))
+
+# Satisfaction
+single_var_analysis.plot_pie(data_preparation.df_yes['Satisfaction'], 's07_satisfaction.pdf',
+    autopct=lambda pct: '{:1.0f}%'.format(pct) if pct > 2.5 else '',    # Don't show values < 2.5%
+    startangle=90, ylabel='', legend=True)
+
--- src/single_var_analysis.py
+++ src/single_var_analysis.py
@@ -1,31 +1,66 @@
+from locale import normalize
 import matplotlib.pyplot as plt
 from matplotlib_venn import venn3
 
 plot_path = 'plots/'
+# colors = ('#e5e1ee', '#dffdff', '#90bede', '#68edc6', '#90f3ff') # Noemi's palette
+colors = ('#5386E4', '#757396', '#949396', '#ABA8B2', '#C3C3C3')
 
-def plot_bar(s, filename, bottom_buffer=0.0, tight=False):
-    ax = s.value_counts().sort_index().plot.bar(rot=45)
-    ax.bar_label(ax.containers[0])      # Add number on top of bars
+# General functions
+def error_msg(arg_name, *args):
+    return f"Invalid value for argument '{arg_name}'. Valid values are: {*args,}."
+
+def plot_bar(s, filename, orientation='v', bar_label=True, sort='index', **kwargs):
+    # Check arguments
+    if sort == 'index':
+        s_sorted = s.value_counts().sort_index()
+    elif sort == 'values':
+        s_sorted = s.value_counts().sort_values()
+    else:
+        raise ValueError(error_msg('sort', 'index', 'values'))
+
+    plt.rcParams.update({'figure.autolayout': True}) # Automatically make all the labels fit
+    kwargs['color'] = colors[0]
+    if orientation == 'v':
+        ax = s_sorted.plot.bar(**kwargs)
+    elif orientation == 'h':
+        ax = s_sorted.plot.barh(**kwargs)
+    else:
+        raise ValueError(error_msg('orientation', 'h', 'v'))
+
+    # Plot the plot 
+    if bar_label: ax.bar_label(ax.containers[0])      # Add number on top of bars
     ax.spines.right.set_visible(False)  # Hide right spine
     ax.spines.top.set_visible(False)    # Hide top spine
-    if tight: plt.tight_layout()
-    if bottom_buffer: plt.subplots_adjust(bottom=bottom_buffer)     # Give bottom space for labels
     plt.savefig(plot_path + filename)
     plt.clf()
+    plt.close()
 
-def response_rate(s):
-    # Calculate the ratio of yes
-    number_of_customers = len(s[s == 'Yes'].index)
-    total_answers = len(s.index)
-
-    customer_percentage = 100 * (number_of_customers / total_answers)
-    print(f'Number of customers: {number_of_customers}/{total_answers} ({customer_percentage:.1f} %)')
-
-    s.value_counts().plot.pie(autopct='%1.1f%%', startangle=90, ylabel='')
-    plt.savefig(plot_path + 's01_response_rate.pdf')
+def plot_hist(s, filename, bar_label=True, bins=4, **kwargs):
+    kwargs['color'] = colors[0]
+    ax = s.plot.hist(bins=bins, **kwargs)
+    ax.set_ylabel('')
+    if bar_label: ax.bar_label(ax.containers[0])      # Add number on top of bars
+    ax.spines.right.set_visible(False)  # Hide right spine
+    ax.spines.top.set_visible(False)    # Hide top spine
+    plt.savefig(plot_path + filename)
     plt.clf()
+    plt.close()
 
-def purchase_location(s):
+def plot_pie(s, filename, legend=False, **kwargs):
+    kwargs['colors'] = colors
+    kwargs['wedgeprops'] = {"edgecolor":"white",'linewidth': 0.5}
+    if legend:
+        kwargs['labels'] = None
+    
+    s.value_counts().plot.pie(**kwargs)
+    if legend: plt.legend(labels=s.value_counts().index, bbox_to_anchor=(0.8,0.8), loc='lower left')
+    plt.savefig(plot_path + filename)
+    plt.clf()
+    plt.close()
+
+# Specific functions
+def purchase_location(s, filename):
     # Plot Venn diagrams
     vc_dict = s.value_counts().to_dict()
     venn3([
@@ -39,11 +74,6 @@ def purchase_location(s):
         ],
         set_labels = ('E-commerce', 'PopUp Store', 'Experience Store'))
 
-    plt.savefig(plot_path + 's03_purchase_location.pdf')
+    plt.savefig(plot_path + filename)
     plt.clf()
-
-def receipt(df):
-    s = df['AvgReceipt']
-    s.plot.hist(bins=6)
-    plt.savefig(plot_path + 's05_receipt.pdf')
-    plt.clf()
+    plt.close()
